261e403c40062f910927285b45a031f00598b996
ShahmarALiyev <saliyev2019@gmail.com>
2022-04-02 13:07:37 -0500

    changing our tests to test against our mongo db database.

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,55 +1,66 @@
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("Test Get /launches", () => {
-  test("it should respond with 200 success", async () => {
-    const response = await request(app)
-      .get("/launches")
-      .expect("Content-Type", /json/)
-      .expect(200);
+describe("Launches API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
-});
-
-describe("Test Post /launches", () => {
-  const completeLaunchData = {
-    mission: "USS",
-    rocket: "NCC",
-    target: "KEPLET",
-    launchDate: "January 4, 2028",
-  };
-
-  const launchDataWithoutDate = {
-    mission: "USS",
-    rocket: "NCC",
-    target: "KEPLET",
-  };
-  const launchDataWithInvalidDate = {
-    mission: "USS",
-    rocket: "NCC",
-    target: "KEPLET",
-    launchDate: "Jzoot",
-  };
-  test("it should respond with 201 success", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(completeLaunchData)
-      .expect("Content-Type", /json/)
-      .expect(201);
-    const requestDate = new Date(completeLaunchData.launchDate).valueOf;
-    const responseDate = new Date(response.body.launchDate).valueOf;
-    expect(response.body).toMatchObject(launchDataWithoutDate);
-    expect(responseDate).toBe(requestDate);
+  afterAll(async () => {
+    await mongoDisconnect;
   });
 
-  test("it should catch invalid dates", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithInvalidDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
+  describe("Test Get /launches", () => {
+    test("it should respond with 200 success", async () => {
+      const response = await request(app)
+        .get("/launches")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
 
-    expect(response.body).toStrictEqual({
-      error: "Invalid launch date",
+  describe("Test Post /launches", () => {
+    const completeLaunchData = {
+      mission: "USS",
+      rocket: "NCC",
+      target: "Kepler-62 f",
+      launchDate: "January 4, 2028",
+    };
+
+    const launchDataWithoutDate = {
+      mission: "USS",
+      rocket: "NCC",
+      target: "Kepler-62 f",
+    };
+    const launchDataWithInvalidDate = {
+      mission: "USS",
+      rocket: "NCC",
+      target: "Kepler-62 f",
+      launchDate: "Jzoot",
+    };
+    test("it should respond with 201 success", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(completeLaunchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf;
+      const responseDate = new Date(response.body.launchDate).valueOf;
+      expect(response.body).toMatchObject(launchDataWithoutDate);
+      expect(responseDate).toBe(requestDate);
+    });
+
+    test("it should catch invalid dates", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDataWithInvalidDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Invalid launch date",
+      });
     });
   });
 });
---
+++ server/src/services/mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const MONGO_URL =
+  "mongodb+srv://nasa:test12345@nasa.siv5u.mongodb.net/Nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
